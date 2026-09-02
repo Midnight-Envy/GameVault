1e790449219f7bd2853126debbb76f751752d8ef
Thomas Correia <thomas.correiap@gmail.com>
2026-09-02 09:03:44 -0400

    Add game browsing and creation

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,29 @@
+class GamesController < ApplicationController
+  def index
+    @games = Game.all
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+
+    if @game.save
+      redirect_to @game
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+private
+
+  def game_params
+    params.require(:game).permit(:name, :developer, :release_year, :genre)
+  end
+end
